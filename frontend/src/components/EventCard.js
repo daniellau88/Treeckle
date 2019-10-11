01913dc755f24622f84f0575c515f62d435a9729
sreesubbash <vostrosubbash97@hotmail.com>
2019-10-12 02:05:14 +0800

    [WIP] Event Modal

--- frontend/src/components/EventCard.js
+++ frontend/src/components/EventCard.js
@@ -1,30 +1,40 @@
 import React, { useState } from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
-import EventModal from '../components/EventModal'
+import { Card, Icon, Image, Button, Header, Modal } from 'semantic-ui-react'
+import { tsPropertySignature } from '@babel/types';
 
 const EventCard = (props) => {
 
     const [modal, setModal] = useState(false);
 
+    const curr = props.event;
+
     return (
-        
+
         <Card>
-            <EventModal open={modal}/>
-            <Image onClick={() => setModal(true)} src='http://www.nusinvest.com/wp-content/uploads/2016/03/General-poster.jpg' wrapped ui={false} />
+            
+            <Modal trigger={<Image onClick={() => setModal(true)} src={curr.image} wrapped ui={false} />} basic>
+                <Modal.Content image>
+                    <Image wrapped size='medium' src={curr.image} />
+                    <Modal.Description>
+                        <Header>{curr.title}</Header>
+                        <p>
+                            {curr.desc}
+                        </p>
+                        <h5>
+                            {curr.location}
+                        </h5>
+                    </Modal.Description>
+
+                </Modal.Content>
+            </Modal>
             <Card.Content>
-                <Card.Header>{props.name}</Card.Header>
+                <Card.Header>{curr.title}</Card.Header>
                 <Card.Meta>
-                    <span className='date'>Joined in 2015</span>
+                    <span className='date'>{curr.date}</span>
                 </Card.Meta>
                 <Card.Description>
-                    Matthew is a musician living in Nashville.
+                    {curr.location}
                 </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <a>
-                    <Icon name='user' />
-                    22 Friends
-                </a>
             </Card.Content>
         </Card>
     );
--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
+import EventCard from '../components/EventCard'
 
-class EventsPage extends React.Component {
-  constructor(props) {
-    super(props);
+const EventsPage = (props) => {
 
-    this.state = {};
-  }
+  const [modal, setModal] = useState(false);
 
-  render() {
-    return (
-      <main className="events-page">
-        <div className="placeholder">This is the events page.</div>
-      </main>
-    );
-  }
-}
+  const eventOne = { 
+    title: 'Investment', 
+    desc: "For investors paying for eac",
+    date: 'Tues Night',
+    location: 'Dining Hall',
+    image: 'http://www.nusinvest.com/wp-content/uploads/2016/03/General-poster.jpg' 
+  };
+
+  return (
+    <main className="events-page">
+      <div className="placeholder">This is the events page.
+        <EventCard event={eventOne} />
+      </div>
+    </main>
+  );
+};
 
 export default EventsPage;
