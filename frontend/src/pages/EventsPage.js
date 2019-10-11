0eaad940a8e0d0b040d0f1dc06b4513c07ef5644
Jeremy <tan_kai_qun97@yahoo.com.sg>
2019-10-12 00:03:39 +0800

    Add pages

---
+++ frontend/src/pages/EventsPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class EventsPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <main className="events-page">
+        <div className="placeholder">This is the events page.</div>
+      </main>
+    );
+  }
+}
+
+export default EventsPage;
