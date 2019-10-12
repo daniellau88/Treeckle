dcb3a7cac149a3a7a6afdf7014284739448a0e34
Jeremy <tan_kai_qun97@yahoo.com.sg>
2019-10-13 04:11:55 +0800

    Fix booking request formatting issues

--- frontend/src/components/BookVenueForm.js
+++ frontend/src/components/BookVenueForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button, Container } from "semantic-ui-react";
+import { Card, Form } from "semantic-ui-react";
 
 class BookVenueForm extends React.Component {
   constructor(props) {
@@ -10,10 +10,11 @@ class BookVenueForm extends React.Component {
 
   render() {
     return (
-      <Card>
+      <Card raised style={{ margin: "0 0 1em 0" }}>
         <Card.Content>
           <Card.Header textAlign="center">Book Seminar Room 5</Card.Header>
         </Card.Content>
+        <Card.Content></Card.Content>
       </Card>
     );
   }
