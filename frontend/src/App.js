27be545c2a51e07454ec1b8fd5b2a6d85ea5940d
sreesubbash <vostrosubbash97@hotmail.com>
2019-10-12 11:17:41 +0800

    minor fixes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-<<<<<<< HEAD
 import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./Navigation";
 
 function App() {
-<<<<<<< HEAD
-  return <EventsPage />;
-=======
   return (
     <Router>
       <Navigation />
     </Router>
   );
->>>>>>> f6932aaf1926d57bbd5a08a6a3a9ac4f214237c0
 }
 
 export default App;
