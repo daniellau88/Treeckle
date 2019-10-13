cec7fde6c8cdadc6c10853d35cdbed98c91f28f8
rssujay <rssujay97@gmail.com>
2019-10-13 13:08:05 +0800

    Add bookingRequest creation endpoint

--- backend/routes/rooms/booking-routes.js
+++ backend/routes/rooms/booking-routes.js
@@ -5,12 +5,38 @@ const { param, body, validationResult } = require('express-validator');
 
 const jsonParser = bodyParser.json();
 
+const checkApprovedOverlaps = async (roomId, start, end) => {
+    const returnObject = {
+        error: 0,
+        overlaps: []
+    }
+    
+    try {
+        const resp = await RoomBooking.find({
+            approved: true, 
+            roomId: roomId, 
+            start: {"$lte": end},
+            end: {"$gte": start},
+        });
+
+        resp.forEach((doc) => {
+            returnObject.overlaps.push({
+                startDate: doc.start.getTime(),
+                endDate: doc.end.getTime()
+            });
+        });
+    } catch(err) {
+        returnObject.error = 1
+    }
+    return returnObject;
+}
+
 //Level 0: Get an array of approved roomBookings' start-end intervals, within a specified range for a particular room
 router.get('/:start-:end', [
     param('roomId').exists(),
     param('start').exists().isInt().toInt(),
     param('end').exists().isInt().toInt()
-    ], (req, res) => {
+    ], async (req, res) => {
         //Check for input errors
         const {roomId,start,end} = req.params;
         const errors = validationResult(req);
@@ -19,56 +45,54 @@ router.get('/:start-:end', [
         } else if (start > end) {
             res.status(422).json({ValueError: "start > end"});
         } else {         
-            RoomBooking.find({
-                approved: true, 
-                roomId: roomId, 
-                start: {"$lte": end},
-                end: {"$gte": start},
-            }, (err, resp) => {
-                if (err) {
-                    res.status(500).send("Database Error");
-                } else {
-                    const response = [];
-                    resp.forEach((doc) => {
-                        response.push({
-                            startDate: doc.start.getTime(),
-                            endDate: doc.end.getTime()
-                        });
-                    });
-                    res.send(response);
-                }
-            });
-        }
+            responseObject = await checkApprovedOverlaps(roomId, start, end);
+
+            if (responseObject.error === 1) {
+                res.status(500).send("Database Error");
+            } else {
+                res.send(responseObject.overlaps);
+            }
+        }   
 });
 
 //Level 0: Create a new bookingRequest
 router.post('/', jsonParser, [
     body('description').exists(),
-    body('start').exists(),
-    body('end').exists(),
-    ], (req, res) => {
+    body('start').exists().isInt(),
+    body('end').exists().isInt(),
+    ], async (req, res) => {
         //Check for input errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             res.status(422).json({ errors: errors.array() });
         } else {
-            const newBookingRequest = {
-                roomId: req.params.roomId,
-                description: req.body.description,
-                createdBy: req.user.userId,
-                start: req.body.start,
-                end: req.body.end,
-                approved: false
-            };
-            new RoomBooking(newBookingRequest).save()
-            .then((result, error) => {
-                if (error) {
-                    res.sendStatus(500);
-                } else {
-                    res.send(result);
+            //Check for overlaps
+            let responseObject = await checkApprovedOverlaps(req.params.roomId, req.body.start, req.body.end);
+
+            if (responseObject.error === 1) {
+                res.status(500).send("Database Error");
+            } else if (responseObject.overlaps.length > 0) {
+                res.status(400).send("Overlaps detected");
+            } else {
+                const newBookingRequest = {
+                    roomId: req.params.roomId,
+                    description: req.body.description,
+                    createdBy: req.user.userId,
+                    start: req.body.start,
+                    end: req.body.end,
+                    approved: false
                 };
-            });
+
+                new RoomBooking(newBookingRequest).save()
+                .then((result, error) => {
+                    if (error) {
+                        res.status(500).send("Database Error");
+                    } else {
+                        res.send(result);
+                    };
+                });
+            }
         }
-});
+    });
 
 module.exports = router;
