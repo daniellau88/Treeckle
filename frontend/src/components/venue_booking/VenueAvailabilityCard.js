d6f0f9011a7d99d9581a0b833cf623d7165d24a5
Jeremy <tan_kai_qun97@yahoo.com.sg>
2019-10-14 02:50:21 +0800

    Add venue availability card skeleton

--- frontend/src/components/venue_booking/VenueAvailabilityCard.js
+++ frontend/src/components/venue_booking/VenueAvailabilityCard.js
@@ -1,58 +1,110 @@
 import React from "react";
-import { Card, Button, Container } from "semantic-ui-react";
+import {
+  Card,
+  Button,
+  Dropdown,
+  Form,
+  Select,
+  Container
+} from "semantic-ui-react";
+
+const roomOptions = [
+  {
+    key: "sr1",
+    text: "Seminar Room 1",
+    value: "Seminar Room 1"
+  },
+  {
+    key: "sr2",
+    text: "Seminar Room 2",
+    value: "Seminar Room 2"
+  },
+  {
+    key: "sr3",
+    text: "Seminar Room 3",
+    value: "Seminar Room 3"
+  },
+  {
+    key: "sr4",
+    text: "Seminar Room 4",
+    value: "Seminar Room 4"
+  },
+  {
+    key: "sr5",
+    text: "Seminar Room 5",
+    value: "Seminar Room 5"
+  },
+  {
+    key: "sr6",
+    text: "Seminar Room 6",
+    value: "Seminar Room 6"
+  }
+];
+
+const weekOptions = [
+  {
+    key: "w9",
+    text: "Week 9",
+    value: "Week 9"
+  },
+  {
+    key: "w10",
+    text: "Week 10",
+    value: "Week 10"
+  },
+  {
+    key: "w11",
+    text: "Week 11",
+    value: "Week 11"
+  },
+  {
+    key: "w12",
+    text: "Week 12",
+    value: "Week 12"
+  },
+  {
+    key: "w13",
+    text: "Week 13",
+    value: "Week 13"
+  }
+];
 
 class VenueAvailabilityCard extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {};
-
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-  }
-
-  handleButtonClick(event, data) {
-    this.setState({ activeButton: data.name });
   }
 
   render() {
-    const { activeButton } = this.state;
-
     return (
       <Card raised style={{ margin: "0 0 1em 0" }}>
         <Card.Content>
-          <Card.Header textAlign="center">Select a venue</Card.Header>
+          <Card.Header textAlign="center">Availability</Card.Header>
         </Card.Content>
         <Card.Content>
-          <Button.Group vertical basic fluid>
-            <Button
-              name="sr"
-              active={activeButton === "sr"}
-              onClick={this.handleButtonClick}
-            >
-              <Container type="text">Seminar Rooms</Container>
-            </Button>
-            <Button
-              name="tr"
-              active={activeButton === "tr"}
-              onClick={this.handleButtonClick}
-            >
-              <Container type="text">Theme Rooms</Container>
-            </Button>
-            <Button
-              name="mpsh"
-              active={activeButton === "mpsh"}
-              onClick={this.handleButtonClick}
-            >
-              <Container type="text">Multi-Purpose Sports Hall</Container>
-            </Button>
-            <Button
-              name="cl"
-              active={activeButton === "cl"}
-              onClick={this.handleButtonClick}
-            >
-              <Container type="text">Common Lounge</Container>
-            </Button>
-          </Button.Group>
+          <Form>
+            <Form.Field
+              control={Select}
+              options={roomOptions}
+              placeholder="Choose room"
+            />
+            <Form.Field
+              control={Select}
+              options={weekOptions}
+              placeholder="Choose week"
+            />
+            <Form.Field label="Monday"></Form.Field>
+            <Form.Field label="Tuesday"></Form.Field>
+            <Form.Field label="Wednesday"></Form.Field>
+            <Form.Field label="Thursday"></Form.Field>
+            <Form.Field label="Friday"></Form.Field>
+            <Form.Field label="Saturday"></Form.Field>
+            <Form.Field label="Sunday"></Form.Field>
+          </Form>
+        </Card.Content>
+        <Card.Content>
+          <Button fluid>Make a booking</Button>
         </Card.Content>
       </Card>
     );
