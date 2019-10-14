17f5dd969372ede20ae6f17f82b17cd6e01abfa6
Jeremy <tan_kai_qun97@yahoo.com.sg>
2019-10-14 21:23:50 +0800

    Integrate backend get categories

--- frontend/src/components/venue_booking/VenueAvailabilityCard.js
+++ frontend/src/components/venue_booking/VenueAvailabilityCard.js
@@ -1,12 +1,9 @@
 import React from "react";
-import {
-  Card,
-  Button,
-  Dropdown,
-  Form,
-  Select,
-  Container
-} from "semantic-ui-react";
+import Axios from "axios";
+import { Card, Button, Form, Table } from "semantic-ui-react";
+import DatePicker from "./DatePicker";
+import "../../styles/VenueAvailabilityCard.scss";
+import { Context } from "../../contexts/UserProvider";
 
 const roomOptions = [
   {
@@ -41,67 +38,270 @@ const roomOptions = [
   }
 ];
 
-const weekOptions = [
+const availabilityOptions = [
   {
-    key: "w9",
-    text: "Week 9",
-    value: "Week 9"
+    time: "12:00 am",
+    availability: "available"
   },
   {
-    key: "w10",
-    text: "Week 10",
-    value: "Week 10"
+    time: "12:30 am",
+    availability: "available"
   },
   {
-    key: "w11",
-    text: "Week 11",
-    value: "Week 11"
+    time: "1:00 am",
+    availability: "available"
   },
   {
-    key: "w12",
-    text: "Week 12",
-    value: "Week 12"
+    time: "1:30 am",
+    availability: "available"
   },
   {
-    key: "w13",
-    text: "Week 13",
-    value: "Week 13"
+    time: "2:00 am",
+    availability: "available"
+  },
+  {
+    time: "2:30 am",
+    availability: "available"
+  },
+  {
+    time: "3:00 am",
+    availability: "available"
+  },
+  {
+    time: "3:30 am",
+    availability: "available"
+  },
+  {
+    time: "4:00 am",
+    availability: "available"
+  },
+  {
+    time: "4:30 am",
+    availability: "available"
+  },
+  {
+    time: "5:00 am",
+    availability: "available"
+  },
+  {
+    time: "5:30 am",
+    availability: "available"
+  },
+  {
+    time: "6:00 am",
+    availability: "available"
+  },
+  {
+    time: "6:30 am",
+    availability: "available"
+  },
+  {
+    time: "7:00 am",
+    availability: "available"
+  },
+  {
+    time: "7:30 am",
+    availability: "available"
+  },
+  {
+    time: "8:00 am",
+    availability: "available"
+  },
+  {
+    time: "8:30 am",
+    availability: "available"
+  },
+  {
+    time: "9:00 am",
+    availability: "available"
+  },
+  {
+    time: "9:30 am",
+    availability: "available"
+  },
+  {
+    time: "10:00 am",
+    availability: "available"
+  },
+  {
+    time: "10:30 am",
+    availability: "available"
+  },
+  {
+    time: "11:00 am",
+    availability: "available"
+  },
+  {
+    time: "11:30 am",
+    availability: "available"
+  },
+  {
+    time: "12:00 pm",
+    availability: "available"
+  },
+  {
+    time: "12:30 pm",
+    availability: "available"
+  },
+  {
+    time: "1:00 pm",
+    availability: "available"
+  },
+  {
+    time: "1:30 pm",
+    availability: "available"
+  },
+  {
+    time: "2:00 pm",
+    availability: "available"
+  },
+  {
+    time: "2:30 pm",
+    availability: "available"
+  },
+  {
+    time: "3:00 pm",
+    availability: "available"
+  },
+  {
+    time: "3:30 pm",
+    availability: "available"
+  },
+  {
+    time: "4:00 pm",
+    availability: "available"
+  },
+  {
+    time: "4:30 pm",
+    availability: "available"
+  },
+  {
+    time: "5:00 pm",
+    availability: "available"
+  },
+  {
+    time: "5:30 pm",
+    availability: "available"
+  },
+  {
+    time: "6:00 pm",
+    availability: "available"
+  },
+  {
+    time: "6:30 pm",
+    availability: "available"
+  },
+  {
+    time: "7:00 pm",
+    availability: "available"
+  },
+  {
+    time: "7:30 pm",
+    availability: "available"
+  },
+  {
+    time: "8:00 pm",
+    availability: "available"
+  },
+  {
+    time: "8:30 pm",
+    availability: "available"
+  },
+  {
+    time: "9:00 pm",
+    availability: "available"
+  },
+  {
+    time: "9:30 pm",
+    availability: "available"
+  },
+  {
+    time: "10:00 pm",
+    availability: "available"
+  },
+  {
+    time: "10:30 pm",
+    availability: "available"
+  },
+  {
+    time: "11:00 pm",
+    availability: "available"
+  },
+  {
+    time: "11:30 pm",
+    availability: "available"
   }
 ];
 
 class VenueAvailabilityCard extends React.Component {
+  static contextType = Context;
+  //this.context.token -> jwt token
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      roomName: "",
+      recommendatedCapacity: "",
+      roomId: "",
+      date: "",
+      availabilityOptions: availabilityOptions
+    };
+
+    this.renderBodyRow = this.renderBodyRow.bind(this);
+    this.onDateChange = this.onDateChange.bind(this);
+  }
+
+  renderBodyRow(data, index) {
+    return (
+      <Table.Row>
+        <Table.Cell>{data.time}</Table.Cell>
+        <Table.Cell
+          positive={data.availability === "available"}
+          negative={data.availability === "unavailable"}
+        >
+          {data.availability}
+        </Table.Cell>
+      </Table.Row>
+    );
+  }
+
+  onDateChange(newDate) {
+    const date = newDate ? newDate : "";
+    this.setState(date);
+    if (date && this.room) {
+    }
+  }
+
+  updateAvailabilityOptions() {
+    Axios.get("/api/rooms/bookings/:roomId/:start-:end");
   }
 
   render() {
     return (
       <Card raised style={{ margin: "0 0 1em 0" }}>
         <Card.Content>
-          <Card.Header textAlign="center">Availability</Card.Header>
+          <Card.Header textAlign="center">View availability</Card.Header>
         </Card.Content>
         <Card.Content>
           <Form>
-            <Form.Field
-              control={Select}
-              options={roomOptions}
-              placeholder="Choose room"
-            />
-            <Form.Field
-              control={Select}
-              options={weekOptions}
-              placeholder="Choose week"
-            />
-            <Form.Field label="Monday"></Form.Field>
-            <Form.Field label="Tuesday"></Form.Field>
-            <Form.Field label="Wednesday"></Form.Field>
-            <Form.Field label="Thursday"></Form.Field>
-            <Form.Field label="Friday"></Form.Field>
-            <Form.Field label="Saturday"></Form.Field>
-            <Form.Field label="Sunday"></Form.Field>
+            <Form.Select options={roomOptions} placeholder="Choose room" />
+            <DatePicker placeholder="Select date" />
           </Form>
+          <div
+            style={{ overflowY: "auto", maxHeight: "19em", marginTop: "1em" }}
+          >
+            <Table
+              celled
+              headerRow={
+                <Table.Row>
+                  <Table.HeaderCell>Time period</Table.HeaderCell>
+                  <Table.HeaderCell>Availability</Table.HeaderCell>
+                </Table.Row>
+              }
+              tableData={availabilityOptions}
+              renderBodyRow={this.renderBodyRow}
+            ></Table>
+          </div>
         </Card.Content>
         <Card.Content>
           <Button fluid>Make a booking</Button>
