ce485057e94c2aaa507b1eb1362b85379dc96c72
sreesubbash <vostrosubbash97@hotmail.com>
2019-10-14 16:06:52 +0800

    Add User Context provider

--- frontend/src/components/LoginDivider.js
+++ frontend/src/components/LoginDivider.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import logo from "../images/treeckle_startup.png";
+import axios from 'axios';
 import {
   Button,
   Divider,
@@ -9,37 +10,63 @@ import {
   Image,
   Header
 } from "semantic-ui-react";
+import UserContext from '../context/UserContext'
 
-const LoginDivider = () => (
-  <Segment placeholder>
-    <Grid columns={2} relaxed="very" stackable>
-      <Grid.Column verticalAlign="middle">
-        <Header style={{ margin: "1.5em auto" }}>Sign in</Header>
-        <Form>
-          <Form.Input icon="user" iconPosition="left" placeholder="Email" />
-          <Form.Input
-            icon="lock"
-            iconPosition="left"
-            placeholder="Password"
-            type="password"
-          />
-          <div style={{ margin: "1em auto" }}>
-            <a href={""}>Forgot password?</a>
-          </div>
-          <Button
-            content="Login"
-            primary
-            style={{ minWidth: "210px", margin: "1em auto" }}
-          />
-        </Form>
-      </Grid.Column>
-      <Grid.Column verticalAlign="middle">
-        <Image src={logo} fluid />
-      </Grid.Column>
-    </Grid>
+const LoginDivider = () => {
 
-    {/* <Divider vertical></Divider> */}
-  </Segment>
-);
+  const { user, setUser } = useContext(UserContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const temp = {
+    name: "admin@admin.com",
+    password: "nothunter2"
+  };
+
+  const login = async () => {
+    console.log(email, password);
+    const res = await axios.post(`https://private-anon-8805d495b4-treeckle.apiary-mock.com/auth/accounts`, { temp });
+    setUser(res.data);
+  }
+
+  return (
+    <Segment placeholder>
+      <Grid columns={2} relaxed="very" stackable>
+        <Grid.Column verticalAlign="middle">
+          <Header style={{ margin: "1.5em auto" }}>Sign in</Header>
+          <Form>
+            <Form.Input icon="user" 
+            iconPosition="left" 
+            placeholder="Email" 
+            onChange={event => setEmail(event.target.value)}
+            />
+            <Form.Input
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+              onChange={event => setPassword(event.target.value)}
+            />
+            <div style={{ margin: "1em auto" }}>
+              <a href={""}>Forgot password?</a>
+            </div>
+            <Button
+              content="Login"
+              primary
+              style={{ minWidth: "210px", margin: "1em auto" }}
+              onClick={login}
+            />
+          </Form>
+        </Grid.Column>
+        <Grid.Column verticalAlign="middle">
+          <Image src={logo} fluid />
+        </Grid.Column>
+      </Grid>
+
+      {/* <Divider vertical></Divider> */}
+    </Segment>
+  );
+}
 
 export default LoginDivider;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Dashboard from "./pages/Dashboard";
 import EventsPage from "./pages/EventsPage";
 import VenueBookingPage from "./pages/VenueBookingPage";
 import ProfilePage from "./pages/ProfilePage";
+import LoginForm from "./pages/authentication/Login";
+import { UserProvider } from './context/UserContext'
 
 export const Routes = () => {
+  const [user, setUser] = useState();
+  
   return (
     <div>
-      <NavigationBar />
-      <Switch>
-        <Route path="/dashboard" component={Dashboard}></Route>
-        <Route path="/events" component={EventsPage}></Route>
-        <Route path="/bookings" component={VenueBookingPage}></Route>
-        <Route path="/profile" component={ProfilePage}></Route>
-      </Switch>
+      <UserProvider value={{user, setUser}}>
+        <NavigationBar />
+        <Switch>
+          <Route path="/dashboard" component={Dashboard}></Route>
+          <Route path="/events" component={EventsPage}></Route>
+          <Route path="/bookings" component={VenueBookingPage}></Route>
+          <Route path="/profile" component={ProfilePage}></Route>
+          <Route path="/login" component={LoginForm}></Route>
+        </Switch>
+      </UserProvider>
     </div>
   );
 };
