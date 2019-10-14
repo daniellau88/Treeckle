dc0ddaf13fc76b48be2eefdd7d8a75609e67e8d8
rssujay <rssujay97@gmail.com>
2019-10-14 17:21:11 +0800

    Modify register endpoint

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -15,34 +15,42 @@ const jsonParser = bodyParser.json();
 router.post("/newAccounts", jsonParser, [
     check('name').exists().isLength({ min: 1 }),
     check('email').isEmail(),
-    check('password').isLength({ min: 8 })
-], (req, res) => {  
+    check('password').isLength({ min: 8 }),
+    check('uniqueURIcomponent').exists()
+], async (req, res) => {  
     //Check for input errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(422).json({ errors: errors.array() });
     } else {
-        //Attempt to register user
-        User.register(new User({
-            email: req.body.email,
-            name: req.body.name,
-            permissionLevel: 0,
-            participatedEventIds: [],
-            subscribedCategories: [],
-            profilePicPath: "" //To modify once created
-        }),
-        req.body.password,
-        err => {
-            if (err) {
-                res.status(400).send({
-                  message: err.name
-                });
-              } else {
-                res.status(200).send({
-                  message: "OK"
+        const relevantReq = await CreateAccount.findOne({ email: req.body.email, uniqueURIcomponent: req.body.uniqueURIcomponent}).lean();
+        if (!relevantReq) {
+            res.status(400).send({
+                message: "Invalid link."
             });
-            }
-        });
+        } else {
+            //Attempt to register user
+            User.register(new User({
+                email: req.body.email,
+                name: req.body.name,
+                permissionLevel: 0,
+                participatedEventIds: [],
+                subscribedCategories: [],
+                profilePicPath: "" //To modify once created
+            }),
+            req.body.password,
+            err => {
+                if (err) {
+                    res.status(400).send({
+                        message: "Invalid link."
+                    });
+                } else {
+                    res.status(200).send({
+                    message: "OK"
+                });
+                }
+            });
+        }
     }
 });
 
@@ -76,9 +84,11 @@ router.post('/newAccountRequest', passport.authenticate('jwt', { session: false 
             if (err) {
                     res.status(500).send("Database Error");
             } else {
-                EmailService.sendText(req.body.email, 'Account Creation for Treeckle', `Dear User, Please proceed with account creation using the following link:\n 
+                EmailService.sendText(req.body.email, 'Account Creation for Treeckle', `Dear User, please proceed with account creation using the following link:\n 
                 https://treeckle.com/auth/newAccounts/${id}\n
-                \n We look forward to having you in our community!`)
+                \n We look forward to having you in our community!\n
+                Yours Sincerely,\n
+                Treeckle Team`)
                 .then(() => {
                     res.sendStatus(200);
                 }).catch(() => {
