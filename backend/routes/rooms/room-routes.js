26b3df24be2dd253bfbb25ea6ce189470b5c876e
rssujay <rssujay97@gmail.com>
2019-10-20 01:36:45 +0800

    Add modify room endpoint

--- backend/routes/rooms/room-routes.js
+++ backend/routes/rooms/room-routes.js
@@ -48,6 +48,43 @@ router.post('/', jsonParser, [
             }
         });
     }
-})
+});
+
+//Admin: Update existing room
+router.patch('/', jsonParser, [
+    body('roomId').exists(),
+    body('name').optional().isString(),
+    body('category').optional().isString(),
+    body('recommendedCapacity').optional().isInt()
+] , async (req, res) => {
+    //Check for input errors
+    const errors = validationResult(req);
+    const permitted = await isPermitted(req.user.role, constants.categories.RoomsManagement, constants.actions.update);
+
+    if (!permitted) {
+        res.sendStatus(401);
+    } else if (!errors.isEmpty()) {
+        res.status(422).json({ errors: errors.array() });
+    }
+
+    Room.findOneAndUpdate({ _id: req.body.roomId }, { 
+        name: req.body.name,
+        category: req.body.category,
+        recommendedCapacity: req.body.recommendedCapacity
+    }, {omitUndefined: true}).lean()
+    .then(result => {
+        if (result) {
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(400);
+        }
+    }).catch(error => {
+        if (error.name === 'CastError') {
+            res.sendStatus(400);
+        } else {
+            res.status(500).send("Database Error");
+        }
+    })
+});
 
 module.exports = router;
