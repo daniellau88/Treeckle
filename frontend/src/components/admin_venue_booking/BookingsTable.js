aa3c7c9a38cbcb471b29562c0982577711c05677
Jeremy <tan_kai_qun97@yahoo.com.sg>
2019-10-25 03:35:09 +0800

    Include more columns

--- frontend/src/components/admin_venue_booking/BookingsTable.js
+++ frontend/src/components/admin_venue_booking/BookingsTable.js
@@ -1,135 +1,9 @@
 import React from "react";
 import Axios from "axios";
-import ReactCollapsingTable from "react-collapsing-table";
 import { Context } from "../../contexts/UserProvider";
 import { Table } from "semantic-ui-react";
 import StatusButton from "./StatusButton";
 import { toDateTimeString } from "../../util/DateUtil";
-
-const fields = [
-  {
-    accessor: "name",
-    label: "Name",
-    priorityLevel: 4,
-    position: 1,
-    minWidth: 150
-  },
-  {
-    accessor: "email",
-    label: "Email",
-    priorityLevel: 7,
-    position: 2,
-    minWidth: 150
-  },
-  {
-    accessor: "contactNum",
-    label: "Contact number",
-    priorityLevel: 8,
-    position: 3,
-    minWidth: 150
-  },
-  {
-    accessor: "venue",
-    label: "Venue",
-    priorityLevel: 3,
-    position: 4,
-    minWidth: 150
-  },
-  {
-    accessor: "start",
-    label: "Start",
-    priorityLevel: 5,
-    position: 5,
-    minWidth: 150
-  },
-  {
-    accessor: "end",
-    label: "End",
-    priorityLevel: 6,
-    position: 6,
-    minWidth: 150
-  },
-  {
-    accessor: "numParticipants",
-    label: "Number of participants",
-    priorityLevel: 9,
-    position: 7,
-    minWidth: 150
-  },
-  {
-    accessor: "purpose",
-    label: "Booking purpose",
-    priorityLevel: 10,
-    position: 8,
-    minWidth: 150
-  },
-  {
-    accessor: "createdDate",
-    label: "Submitted at",
-    priorityLevel: 2,
-    position: 9,
-    minWidth: 150
-  },
-  {
-    accessor: "status",
-    label: "Status",
-    priorityLevel: 1,
-    position: 10,
-    CustomComponent: StatusButton,
-    minWidth: 150
-  }
-];
-
-const rows = [
-  {
-    id: 1,
-    name: "Jeremy",
-    email: "jeremytan97@u.nus.edu",
-    contactNum: "81217370",
-    venue: "Multi-Purpose Sports Hall",
-    start: "25/12/2019 7.00 PM",
-    end: "26/12/2019 11.00 AM",
-    numParticipants: "20",
-    purpose: "I want to play badminton",
-    status: "booked"
-  },
-  {
-    id: 2,
-    name: "Jeremy",
-    email: "jeremytan97@u.nus.edu",
-    contactNum: "81217370",
-    venue: "Multi-Purpose Sports Hall",
-    start: "25/12/2019 7.00 PM",
-    end: "26/12/2019 11.00 AM",
-    numParticipants: "20",
-    purpose: "I want to play badminton",
-    status: "booked"
-  },
-  {
-    id: 3,
-    name: "Jeremy",
-    email: "jeremytan97@u.nus.edu",
-    contactNum: "81217370",
-    venue: "Multi-Purpose Sports Hall",
-    start: "25/12/2019 7.00 PM",
-    end: "26/12/2019 11.00 AM",
-    numParticipants: "20",
-    purpose: "I want to play badminton",
-    status: "booked"
-  },
-  {
-    id: 4,
-    name: "Jeremy dsdasdfdsfdsfsdfsdfsdasf",
-    email: "jeremytan97@u.nus.edu",
-    contactNum: "81217370",
-    venue: "Multi-Purpose Sports Hall",
-    start: "25/12/2019 7.00 PM",
-    end: "26/12/2019 11.00 AM",
-    numParticipants: "20",
-    purpose: "I want to play badminton",
-    status: "booked"
-  }
-];
 
 class BookingsTable extends React.Component {
   static contextType = Context;
@@ -144,7 +18,7 @@ class BookingsTable extends React.Component {
       cancelledRequests: []
     };
 
-    //this.renderBodyRow = this.renderBodyRow.bind(this);
+    this.renderBodyRow = this.renderBodyRow.bind(this);
     this.retrieveAllRequests = this.retrieveAllRequests.bind(this);
   }
 
@@ -190,84 +64,18 @@ class BookingsTable extends React.Component {
             ...cancelledRequests
           ];
           this.setState({
-            allRequests: this.renderRows(allRequests)
+            allRequests,
+            pendingRequests,
+            approvedRequests,
+            rejectedRequests,
+            cancelledRequests
           });
         }
       )
     );
   }
 
-  renderRows(allRequests) {
-    return allRequests.map(request => {
-      const {
-        createdByName,
-        createdByEmail,
-        roomName,
-        start,
-        end,
-        description,
-        createdDate,
-        approved,
-        bookingId
-      } = request;
-      const status = approved;
-      const [contactNum, numParticipants, purpose] = description.split("\n");
-      console.log(status);
-      return {
-        name: createdByName,
-        email: createdByEmail,
-        contactNum: contactNum,
-        venue: roomName,
-        start: toDateTimeString(start),
-        end: toDateTimeString(end),
-        numParticipants: numParticipants,
-        purpose: purpose,
-        createdDate: toDateTimeString(createdDate),
-        status: (
-          <StatusButton
-            status={status}
-            cancellable={false}
-            bookingId={bookingId}
-            updateTable={this.retrieveAllRequests}
-          />
-        )
-      };
-    });
-  }
-
-  render() {
-    return (
-      <ReactCollapsingTable
-        rows={this.state.allRequests}
-        columns={fields}
-        theme="table ui"
-      />
-    );
-  }
-}
-
-export default BookingsTable;
-/*
-<Table
-        fixed
-        headerRow={
-          <Table.Row>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Email</Table.HeaderCell>
-            <Table.HeaderCell>Venue</Table.HeaderCell>
-            <Table.HeaderCell>Start</Table.HeaderCell>
-            <Table.HeaderCell>End</Table.HeaderCell>
-            <Table.HeaderCell>Description</Table.HeaderCell>
-            <Table.HeaderCell>Submitted at</Table.HeaderCell>
-            <Table.HeaderCell>Status</Table.HeaderCell>
-          </Table.Row>
-        }
-        tableData={this.state.allRequests}
-        renderBodyRow={this.renderBodyRow}
-      />
-      
-
-      renderBodyRow(data, index) {
+  renderBodyRow(data, index) {
     console.log(data);
     const {
       createdByName,
@@ -281,14 +89,17 @@ export default BookingsTable;
       bookingId
     } = data;
     const status = approved;
+    const [contactNum, numParticipants, purpose] = description.split("\n");
     const row = (
       <Table.Row>
         <Table.Cell>{createdByName}</Table.Cell>
         <Table.Cell>{createdByEmail}</Table.Cell>
+        <Table.Cell>{contactNum}</Table.Cell>
         <Table.Cell>{roomName}</Table.Cell>
         <Table.Cell>{toDateTimeString(start)}</Table.Cell>
         <Table.Cell>{toDateTimeString(end)}</Table.Cell>
-        <Table.Cell>{description}</Table.Cell>
+        <Table.Cell>{numParticipants}</Table.Cell>
+        <Table.Cell>{purpose}</Table.Cell>
         <Table.Cell>{toDateTimeString(createdDate)}</Table.Cell>
         <Table.Cell>
           <StatusButton
@@ -302,4 +113,29 @@ export default BookingsTable;
     );
     return row;
   }
-      */
+
+  render() {
+    return (
+      <Table
+        headerRow={
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Email</Table.HeaderCell>
+            <Table.HeaderCell>Contact number</Table.HeaderCell>
+            <Table.HeaderCell>Venue</Table.HeaderCell>
+            <Table.HeaderCell>Start</Table.HeaderCell>
+            <Table.HeaderCell>End</Table.HeaderCell>
+            <Table.HeaderCell>Number of participants</Table.HeaderCell>
+            <Table.HeaderCell>Purpose</Table.HeaderCell>
+            <Table.HeaderCell>Submitted at</Table.HeaderCell>
+            <Table.HeaderCell>Status</Table.HeaderCell>
+          </Table.Row>
+        }
+        tableData={this.state.allRequests}
+        renderBodyRow={this.renderBodyRow}
+      />
+    );
+  }
+}
+
+export default BookingsTable;
