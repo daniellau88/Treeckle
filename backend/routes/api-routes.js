80961871b5328f0dfcbc384f848c030d3d98ae1c
sreesubbash <vostrosubbash97@hotmail.com>
2019-10-26 12:25:34 +0800

    Skeleton for events route

--- backend/routes/api-routes.js
+++ backend/routes/api-routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const roomRoutes = require('./rooms/room-routes');
+const eventRoutes = require('./events/event-routes');
 const accountRoutes = require('./account-routes');
 const emailRoutes = require('./email-routes');
 
 router.use('/rooms', roomRoutes);
 router.use('/accounts', accountRoutes);
-router.use('/emails', emailRoutes)
+router.use('/emails', emailRoutes);
+router.use('/events', eventRoutes);
 
 module.exports = router;
