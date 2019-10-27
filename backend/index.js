6ac81503f0b1c81e9dc3b05e3f9a86f9e0048638
rssujay <rssujay97@gmail.com>
2019-10-27 16:06:15 +0800

    Modify for production

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const serveIndex = require('serve-index');
 
 //Initialize
 app.use(passport.initialize());
-configurePermissions();
+//configurePermissions();
 
 //Connect to DB
 mongoose.connect(
@@ -45,7 +45,7 @@ app.get("/", (req, res) =>
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-//app.use('/ftp', express.static('public'), serveIndex('public', {'icons': true}));
+app.use('/ftp', express.static('public'), serveIndex('public', {'icons': true}));
 
 //Catch GET requests to invalid URIs and redirect to home page
 app.get("/*", (req, res) => {
