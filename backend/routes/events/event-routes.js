2aa585890bd8c4d1193c75b2744ba1a939dd343c
sreesubbash <vostrosubbash97@hotmail.com>
2019-10-27 08:17:41 +0800

    API to set user categories and fetch by categories

--- backend/routes/events/event-routes.js
+++ backend/routes/events/event-routes.js
@@ -3,6 +3,7 @@ const creationRoutes = require('./creation-routes');
 const attendRoutes = require('./attend-routes');
 const bodyParser = require('body-parser');
 const Event = require('../../models/events-model');
+const User = require('../../models/authentication/user-model');
 const constants = require('../../config/constants');
 const { isPermitted } = require('../../services/auth-service');
 const { body, validationResult } = require('express-validator');
@@ -18,13 +19,22 @@ router.post('/', jsonParser, [
 ], async (req, res) => {
     //Check for input errors
 
+    //Add to categories to a categories model
+
     const newEvent = {
         title: req.body.title,
+        description: req.body.description,
+        categories: req.body.categories,
+        capacity: req.body.capacity,
+        organisedBy: req.body.organisedBy,
+        venue: req.body.venue,
+        signupsAllowed: req.body.signupsAllowed,
+        attendees: [],
         createdBy: req.user.userId,
         eventDate: req.body.date,
     };
 
-    console.log(req.body.title + "-" + req.body.date);
+    //console.log(req.body.title + "-" + req.body.date);
 
     const event = Event.byTenant(req.user.residence);
     const eventInstance = new event(newEvent);
@@ -77,6 +87,53 @@ router.post('/all', jsonParser, [
             res.status(500).send("Database Error");
         });
 });
+
+//Resident: Set category tags
+router.post('/set/tags', jsonParser, [
+], async (req, res) => {
+    //Check for input errors
+    //console.log(req.body.categories);
+    User.findOneAndUpdate({ _id: req.user.userId }, { subscribedCategories: req.body.categories })
+        .then(resp => {
+            res.status(200).send();
+        })
+        .catch(err => {
+            res.sendStatus(400);
+        })
+
+});
+
+//Resident: View events with user categories
+router.post('/all/tags', jsonParser, [
+], async (req, res) => {
+    //Check for input errors
+
+    User.findOne({ _id: req.user.userId }).lean()
+        .then(user => {
+            const tags = user.subscribedCategories;
+            Event.byTenant(req.user.residence).find({}).lean()
+                .then(async resp => {
+                    try {
+                        const sendToUser = [];
+                        resp.forEach(event => {
+                            if (tags.some(x => event.categories.includes(x))) {
+                                sendToUser.push({
+                                    event
+                                });
+                            }
+                        });
+                        res.send(sendToUser);
+                    } catch (err) {
+                        res.status(500).send("Database Error");
+                    }
+                }).catch(err => {
+                    res.status(500).send("Database Error");
+                });
+        });
+
+
+});
+
 
 //delete event by title
 router.delete('/', jsonParser, [
