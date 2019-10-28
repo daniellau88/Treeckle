ac2a5ec648b498cfe38a79b667b30e7cb7539564
rssujay <rssujay97@gmail.com>
2019-10-28 11:31:43 +0800

    Enable query customisation and endpoints fix

--- backend/routes/events/gallery-routes.js
+++ backend/routes/events/gallery-routes.js
@@ -1,22 +1,30 @@
 const router = require('express').Router();
 const {isPermitted} = require('../../services/auth-service');
 const Event = require('../../models/events-model');
+const User = require('../../models/authentication/user-model');
 const constants = require('../../config/constants');
-const { body, validationResult } = require('express-validator');
+const { body, query, validationResult } = require('express-validator');
 const bodyParser = require('body-parser');
 
 const jsonParser = bodyParser.json();
 
 //Resident and higher: View all events
-router.get('/', async (req, res) => {
+router.get('/', [
+    //query('historical').optional().isBoolean().toBoolean(), // Don't support historical unless required in the future
+    query('latestFirst').optional().isBoolean().toBoolean()
+], async (req, res) => {
     const permitted = await isPermitted(req.user.role, constants.categories.eventEngagement, constants.actions.read);
 
     if (!permitted) {
         res.sendStatus(401);
-        return ;
+        return;
     }
 
-    Event.byTenant(req.user.residence).find({}, '-creationDate -createdBy -__v -tenantId', {sort: {eventDate : 1}}).lean()
+    //const filter = (req.query.historical)? {} : { eventDate : { $gte : Date.now() }};
+    const filter =  { eventDate: {$gte : Date.now() }};
+    const sortOrder = (req.query.latestFirst)? {sort: {eventDate : -1}} : {sort: {eventDate : 1}};
+
+    Event.byTenant(req.user.residence).find(filter, '-creationDate -createdBy -__v -tenantId', sortOrder ).lean()
         .then(resp => {
             const sendToUser = [];
             resp.forEach(doc => {
@@ -59,16 +67,23 @@ router.patch('/', jsonParser, [
         res.status(422).json({ errors: errors.array() });
     } else {
         const updateObject = (req.body.signUp === 1)? { $addToSet : { attendees: req.user.userId }} : { $pull : { attendees: req.user.userId }};
+        const userUpdateObject = (req.body.signUp === 1)? { $addToSet : { participatedEventsIds : req.body.eventId } }
+            : { $pull : {participatedEventsIds : req.body.eventId } };
 
-        Event.byTenant(req.user.residence).findOneAndUpdate({ _id: req.body.eventId, signupsAllowed: true }, updateObject)
-        .then(result => {
+        Event.byTenant(req.user.residence).findOneAndUpdate({ _id: req.body.eventId, signupsAllowed: true, eventDate: { $gte : Date.now() }}, updateObject)
+        .then(async result => {
             if (!result) {
                 res.sendStatus(400);
             } else {
-                res.send({
-                    counterAddition: (req.body.signUp === 1)? (result.attendees.includes(req.user.userId))? 0 : 1 
-                        : (result.attendees.includes(req.user.userId))? -1 : 0
-                });
+                try {   
+                    await User.updateOne({ _id: req.user.userId }, userUpdateObject).lean();
+                    res.send({
+                        counterAddition: (req.body.signUp === 1)? (result.attendees.includes(req.user.userId))? 0 : 1 
+                            : (result.attendees.includes(req.user.userId))? -1 : 0
+                    });
+                } catch {
+                    res.status(500).send("Database Error");
+                }  
             }
         })
         .catch (err => {
