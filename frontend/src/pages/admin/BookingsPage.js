b409fde00ccf5959b5eb7ffdeea330b3b91ab919
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-01-02 16:11:30 +0800

    Add manage booking button

--- frontend/src/pages/admin/BookingsPage.js
+++ frontend/src/pages/admin/BookingsPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactGA from "react-ga";
 import { Context } from "../../contexts/UserProvider";
-import { Container, Menu } from "semantic-ui-react";
+import { Container, Menu, Button } from "semantic-ui-react";
 import BookingsTable from "../../components/admin/venue_booking/BookingsTable";
 
 class BookingsPage extends React.Component {
@@ -19,7 +19,12 @@ class BookingsPage extends React.Component {
         <br />
         <br />
         <Container>
-          <h1 style={{ color: "#FDFDFD" }}>Booking Requests</h1>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <h1 style={{ color: "#FDFDFD" }}>Booking Requests</h1>
+            <span>
+              <Button>Manage</Button>
+            </span>
+          </div>
           <BookingsTable />
         </Container>
         <br />
