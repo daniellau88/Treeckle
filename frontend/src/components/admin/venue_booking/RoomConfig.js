df9b6d8ef6529e317123f86519e9ee0ebc22eae8
LOVELELESS <koh_rayson@yahoo.com.sg>
2020-01-09 05:04:53 +0800

    Added React Hooks

--- frontend/src/components/admin/venue_booking/RoomConfig.js
+++ frontend/src/components/admin/venue_booking/RoomConfig.js
@@ -1,128 +1,123 @@
-import React from "react";
-import axios from "axios"; import { Context } from "../../../contexts/UserProvider"; import { Table, Segment, Button } from "semantic-ui-react"; import StatusButton from "../../common/StatusButton";
-import DeleteRoomButton from "./DeleteRoomButton";
-import { toDateTimeString } from "../../../util/DateUtil";
-import { CONSOLE_LOGGING } from "../../../DevelopmentView";
-import "../../../styles/ScrollableTable.scss";
+import React, {useState, useEffect, useContext} from 'react';
+import axios from 'axios';
+import {Context} from '../../../contexts/UserProvider';
+import {Table, Segment, Button} from 'semantic-ui-react';
+import DeleteRoomButton from './DeleteRoomButton';
+import {CONSOLE_LOGGING} from '../../../DevelopmentView';
+import '../../../styles/ScrollableTable.scss';
 
-class RoomConfig extends React.Component {
-    static contextType = Context;
+function RoomConfig(props) {
+  const {token, name, profilePic, role, setUser, resetUser} = useContext(
+    Context,
+  );
+  const [allCategories, setAllCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            allCategories: [],
-            isLoading: true
-        };
+  useEffect(() => {
+    updateAllCategories();
+  }, []);
 
-        this.renderBodyRow = this.renderBodyRow.bind(this);
-        this.updateAllCategories = this.updateAllCategories.bind(this);
-    }
+  function updateAllCategories() {
+    axios
+      .all([getAllCategories()])
+      .then(
+        axios.spread(categoriesArr => {
+          const newAllCategories = [];
+          axios.all([getRoomsData(categoriesArr, newAllCategories)]).then(
+            axios.spread(res => {
+              allCategories.sort();
+              setAllCategories(newAllCategories);
+              setIsLoading(false);
+            }),
+          );
+        }),
+      )
+      .catch(err => console.log(err));
+  }
 
-    componentDidMount() {
-        this.updateAllCategories();
-    };
+  function getAllCategories() {
+    return axios
+      .get('../api/rooms/categories', {
+        headers: {Authorization: `Bearer ${token}`},
+      })
+      .then(res => {
+        CONSOLE_LOGGING &&
+          console.log(`GET request to /api/rooms/categories: ${res}`);
+        return res.data.categories;
+      })
+      .catch(err => console.log(err));
+  }
 
-    updateAllCategories() {
-        axios.all([this.getAllCategories()])
-            .then(axios.spread(categoriesArr => {
-                const allCategories = [];
-                axios.all([this.getRoomsData(categoriesArr, allCategories)])
-                    .then(axios.spread(res => {
-                        allCategories.sort();
-                        this.setState({ 
-                            allCategories,
-                            isLoading: false
-                        });
-                    }));
-            }))
-            .catch(err => console.log(err));
-    }
+  function getRoomsData(categoriesArr, resArr) {
+    const promiseArr = [];
+    categoriesArr.forEach(cat => {
+      promiseArr.push(
+        axios
+          .get(`../api/rooms/categories/${cat}`, {
+            headers: {Authorization: `Bearer ${token}`},
+          })
+          .then(rooms => {
+            CONSOLE_LOGGING &&
+              console.log(
+                `GET request to /api/rooms/categories/${cat}: ${rooms}`,
+              );
+            const roomsData = rooms.data;
+            roomsData.map(room => {
+              const {roomId, name, recommendedCapacity, contactEmail} = room;
 
-    getAllCategories() {
-        return axios.get('../api/rooms/categories', { headers: { Authorization: `Bearer ${this.context.token}` } })
-            .then(res => res.data.categories)
-            .catch(err => console.log(err));
-    }
+              resArr.push({
+                category: cat,
+                roomId,
+                name,
+                recommendedCapacity,
+                contactEmail,
+              });
+            });
+          }),
+      );
+    });
+    return Promise.all(promiseArr);
+  }
 
-    getRoomsData(categoriesArr, resArr) {
-        const promiseArr = [];
-        categoriesArr.forEach(cat => {
-            promiseArr.push(axios.get(`../api/rooms/categories/${cat}`, { headers: { Authorization: `Bearer ${this.context.token}` } })
-                .then(rooms => {
-                    const roomsData = rooms.data;
-                    roomsData.map(room => {
-                        const {
-                            roomId,
-                            name,
-                            recommendedCapacity,
-                            contactEmail
-                        } = room;
+  function renderBodyRow(data, index) {
+    const {category, roomId, name, recommendedCapacity, contactEmail} = data;
 
-                        resArr.push({
-                            category: cat,
-                            roomId,
-                            name,
-                            recommendedCapacity,
-                            contactEmail
-                        });
-                    });
-                }));
-        });
-        return Promise.all(promiseArr);
-    }
+    const row = (
+      <Table.Row>
+        <Table.Cell>{category}</Table.Cell>
+        <Table.Cell>{name}</Table.Cell>
+        <Table.Cell>{contactEmail}</Table.Cell>
+        <Table.Cell>
+          <DeleteRoomButton id={roomId} updateTable={updateAllCategories} />
+        </Table.Cell>
+      </Table.Row>
+    );
+    return row;
+  }
 
-
-    renderBodyRow(data, index) {
-        const {
-            category,
-            roomId,
-            name,
-            recommendedCapacity,
-            contactEmail
-        } = data;
-
-        const row = (
+  return (
+    <div className="scrollable-table" style={{maxHeight: '44em'}}>
+      {allCategories.length > 0 ? (
+        <Table
+          selectable
+          headerRow={
             <Table.Row>
-                <Table.Cell>{category}</Table.Cell>
-                <Table.Cell>{name}</Table.Cell>
-                <Table.Cell>{contactEmail}</Table.Cell>
-                <Table.Cell><DeleteRoomButton id={roomId} updateTable={this.updateAllCategories} /></Table.Cell>
+              <Table.HeaderCell>Category</Table.HeaderCell>
+              <Table.HeaderCell>Room Name</Table.HeaderCell>
+              <Table.HeaderCell>Contact Email</Table.HeaderCell>
+              <Table.HeaderCell>Actions</Table.HeaderCell>
             </Table.Row>
-        );
-        return row;
-    }
-
-    render() {
-        return (
-            <div className="scrollable-table" style={{ maxHeight: "44em" }}>
-                {this.state.allCategories.length > 0 ? (
-                    <Table
-                        selectable
-                        headerRow={
-                            <Table.Row>
-                                <Table.HeaderCell>Category</Table.HeaderCell>
-                                <Table.HeaderCell>Room Name</Table.HeaderCell>
-                                <Table.HeaderCell>Contact Email</Table.HeaderCell>
-                                <Table.HeaderCell>Actions</Table.HeaderCell>
-                            </Table.Row>
-                        }
-                        tableData={this.state.allCategories}
-                        renderBodyRow={this.renderBodyRow}
-                    />
-                ) : (
-                    <Segment
-                        placeholder
-                        textAlign="center"
-                        size="huge"
-                        loading={this.state.isLoading}
-                    >
-                        There are currently no booking requests
-                    </Segment>
-                )}
-            </div>
-        );
-    }
+          }
+          tableData={allCategories}
+          renderBodyRow={renderBodyRow}
+        />
+      ) : (
+        <Segment placeholder textAlign="center" size="huge" loading={isLoading}>
+          There are currently no rooms configured
+        </Segment>
+      )}
+    </div>
+  );
 }
 
 export default RoomConfig;
--- frontend/src/components/admin/venue_booking/RoomCreationForm.js
+++ frontend/src/components/admin/venue_booking/RoomCreationForm.js
@@ -1,220 +1,211 @@
-import React from 'react'
-import axios from "axios";
-import { Container, Button, Checkbox, Form, Confirm } from 'semantic-ui-react'
-import StatusBar from "../../common/StatusBar";
-import { Context } from "../../../contexts/UserProvider";
-import { CONSOLE_LOGGING } from "../../../DevelopmentView";
+import React, {useContext, useState, useEffect} from 'react';
+import axios from 'axios';
+import {Container, Button, Checkbox, Form, Confirm} from 'semantic-ui-react';
+import StatusBar from '../../common/StatusBar';
+import {Context} from '../../../contexts/UserProvider';
+import {CONSOLE_LOGGING} from '../../../DevelopmentView';
 
-const SUCCESS_MSG = "New Room has been successfully made.";
-const OVERLAP_CONFLICT_MSG = 
-    "This room is has been created already."; 
-const UNKNOWN_ERROR_MSG = "An unknown error has occurred. Please try again.";
-const otherOption = { text: 'other', value: 'other' }; 
-    
+const SUCCESS_MSG = 'New Room has been successfully made.';
+const OVERLAP_CONFLICT_MSG = 'This room is has been created already.';
+const UNKNOWN_ERROR_MSG = 'An unknown error has occurred. Please try again.';
+const otherOption = {text: 'other', value: 'other'};
 
+function RoomCreationForm(props) {
+  const {token, name, profilePic, role, setUser, resetUser} = useContext(
+    Context,
+  );
 
-class RoomCreationForm extends React.Component {
-    static contextType = Context;
-    constructor(props) {
-        super(props);
-        this.state = this.getInitialState();
+  const [status, setStatus] = useState(null);
+  const [submitting, setSubmitting] = useState(false);
+  const [confirming, setConfirming] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [options, setOptions] = useState(null);
 
-        this.onChange = this.onChange.bind(this);
-        this.onSubmitting = this.onSubmitting.bind(this);
-        this.handleOnSubmit = this.handleOnSubmit.bind(this);
-        this.toggleConfirmation = this.toggleConfirmation.bind(this);
-    }
+  const [roomName, setRoomName] = useState(null);
+  const [roomCategory, setRoomCategory] = useState(null);
+  const [roomNewCategory, setRoomNewCategory] = useState(null);
+  const [roomRecommendedCapacity, setRoomRecommendedCapacity] = useState(null);
+  const [roomContactEmail, setRoomContactEmail] = useState(null);
 
-    getInitialState() {
-        const initialState = {
-            confirming: false,
-            roomName: null,
-            roomCategory: null,
-            options: null,
-            roomNewCategory: null,
-            roomRecommendedCapacity: null,
-            roomContactEmail: null,
-            success: false,
-            status: null,
-            submitting: false
+  useEffect(() => {
+    axios
+      .get('../api/rooms/categories', {
+        headers: {Authorization: `Bearer ${token}`},
+      })
+      .then(res => {
+        const categories = res.data.categories;
+        const newOptions = [];
+        categories.forEach(cat => {
+          const tempData = {
+            text: cat,
+            value: cat,
+          };
+          newOptions.push(tempData);
+        });
+        newOptions.push(otherOption);
+        setOptions(newOptions);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  function resetState() {
+    setStatus(null);
+    setSubmitting(false);
+    setConfirming(false);
+    setSuccess(false);
+    setRoomName(null);
+    setRoomCategory(null);
+    setOptions(null);
+    setRoomNewCategory(null);
+    setRoomContactEmail(null);
+  }
+
+  // all fields cannot be empty
+  function areValidFields() {
+    return (
+      roomName &&
+      (roomCategory === 'other' ? roomNewCategory : roomCategory) &&
+      roomRecommendedCapacity &&
+      roomContactEmail
+    );
+  }
+
+  async function onSubmitting() {
+    toggleConfirmation();
+    toggleStatusBar(true);
+  }
+
+  function handleOnSubmit() {
+    onSubmitting()
+      .then(() => {
+        let category;
+        if (roomCategory === 'other') {
+          category = roomNewCategory;
+        } else {
+          category = roomCategory;
+        }
+        const data = {
+          name: roomName,
+          category: category,
+          recommendedCapacity: roomRecommendedCapacity,
+          contactEmail: roomContactEmail,
         };
-        return initialState;
-    }
+        axios
+          .post('../api/rooms', data, {
+            headers: {Authorization: `Bearer ${token}`},
+          })
+          .then(response => {
+            CONSOLE_LOGGING && console.log('POST form submission:', response);
+            if (response.status === 200) {
+              setSuccess(true);
+              renderStatusBar(true, SUCCESS_MSG);
+            }
+          })
+          .catch(({response}) => {
+            CONSOLE_LOGGING &&
+              console.log('POST form submission error:', response);
+            let msg;
+            switch (response.status) {
+              case 400:
+                msg = OVERLAP_CONFLICT_MSG;
+                break;
+              case 401:
+                alert('Your current session has expired. Please log in again.');
+                resetUser();
+                break;
+              default:
+                msg = UNKNOWN_ERROR_MSG;
+            }
+            renderStatusBar(false, msg);
+          });
+      })
+      .then(() => {
+        toggleStatusBar(false);
+      });
+  }
 
-    resetState() {
-        this.setState(this.getInitialState());
-    }
+  function toggleConfirmation() {
+    setConfirming(!confirming);
+  }
 
-    componentDidMount() {
-        axios.get('../api/rooms/categories', { headers: { Authorization: `Bearer ${this.context.token}` } })
-            .then(res => {
-                const categories = res.data.categories;
-                const options = [];
-                categories.forEach(cat => {
-                    const tempData = {
-                        text: cat, value: cat
-                    };
-                    options.push(tempData);
-                })
-                options.push(otherOption);
-                this.setState({ options: options });
-            })
-            .catch(err => console.log(err));
-    }
+  function toggleStatusBar(newSubmitting) {
+    setSubmitting(newSubmitting);
+  }
 
-    // all fields cannot be empty
-    areValidFields() {
-        return (
-            this.state.roomName &&
-            (this.state.roomCategory === 'other' ? this.state.roomNewCategory : this.state.roomCategory) &&
-            this.state.roomRecommendedCapacity &&
-            this.state.roomContactEmail 
-        );
-    }
+  function renderStatusBar(success, message) {
+    const newStatus = {
+      success: success,
+      message: message,
+    };
+    setStatus(newStatus);
+    CONSOLE_LOGGING && console.log('Status:', status);
+  }
 
-    onChange(event, { name, value }) {
-        CONSOLE_LOGGING && console.log(`${name} changed:`, value);
-        this.setState({ [name]: value });
-    }
-
-    async onSubmitting() {
-        this.toggleConfirmation();
-        this.toggleStatusBar(true);
-    }
-
-    handleOnSubmit() {
-        this.onSubmitting()
-            .then(() => {
-                let category; 
-                if (this.state.roomCategory === 'other') {
-                    category = this.state.roomNewCategory;
-                } else {
-                    category = this.state.roomCategory;
-                }
-                const data = {
-                    name: this.state.roomName,
-                    category: category,
-                    recommendedCapacity: this.state.roomRecommendedCapacity,
-                    contactEmail: this.state.roomContactEmail
-                };
-                axios
-                    .post("../api/rooms", data, {
-                        headers: { Authorization: `Bearer ${this.context.token}` }
-                    })
-                    .then(response => {
-                        CONSOLE_LOGGING && console.log("POST form submission:", response);
-                        if (response.status === 200) {
-                            this.setState({ success: true });
-                            this.renderStatusBar(true, SUCCESS_MSG);
-                        }
-                    })
-                    .catch(({ response }) => {
-                        CONSOLE_LOGGING &&
-                            console.log("POST form submission error:", response);
-                        let msg;
-                        switch (response.status) {
-                            case 400:
-                                msg = OVERLAP_CONFLICT_MSG;
-                                break;
-                            case 401:
-                                alert("Your current session has expired. Please log in again.");
-                                this.context.resetUser();
-                                break;
-                            default:
-                                msg = UNKNOWN_ERROR_MSG;
-                        }
-                        this.renderStatusBar(false, msg);
-                    });
-            })
-            .then(() => {
-                this.toggleStatusBar(false);
-            });
-    }
-
-    toggleConfirmation() {
-        this.setState({ confirming: !this.state.confirming });
-    }
-
-    toggleStatusBar(submitting) {
-        this.setState({ submitting });
-    }
-
-    renderStatusBar(success, message) {
-        const status = {
-            success: success,
-            message: message
-        };
-        this.setState({ status });
-        CONSOLE_LOGGING && console.log("Status:", status);
-    }
-
-    render() {
-        return (
-            <Container className="scrollable-table" style={{ padding: "2em" }}>
-                {(this.state.status || this.state.submitting) && (
-                    <div>
-                        <StatusBar
-                            status={this.state.status}
-                            submitting={this.state.submitting}
-                        />
-                        <br />
-                    </div>
-                )}
-                <Form>
-                    <Form.Input
-                        label='Room Name' 
-                        onChange={this.onChange}
-                        required
-                        name='roomName'
-                    />
-                    <Form.Select
-                        options={this.state.options}
-                        label='Room Category'
-                        onChange={this.onChange}
-                        required
-                        name='roomCategory'
-                    />
-                    {(this.state.roomCategory==='other') && (
-                        <Form.Input
-                            placeholder='New Room Category'
-                            onChange={this.onChange}
-                            required
-                            name='roomNewCategory'
-                        />
-                    )}
-                    <Form.Input
-                        type='number'
-                        label='Room Recommended Capacity'
-                        onChange={this.onChange}
-                        required
-                        name='roomRecommendedCapacity'
-                    />
-                    <Form.Input
-                        type='email'
-                        label='Room Contact Email'
-                        onChange={this.onChange}
-                        required
-                        name='roomContactEmail'
-                    />
-                    <Button
-                        primary
-                        fluid
-                        disabled={!this.areValidFields() || this.state.success}
-                        onClick={this.toggleConfirmation}
-                    >
-                        Submit
-                    </Button>
-                    <Confirm
-                        open={this.state.confirming}
-                        onCancel={this.toggleConfirmation}
-                        onConfirm={this.handleOnSubmit}
-                        content="Confirm Room Creation?"
-                        size="mini"
-                    />
-                </Form>
-            </Container>
-        );
-    }
+  return (
+    <Container className="scrollable-table" style={{padding: '2em'}}>
+      {(status || submitting) && (
+        <div>
+          <StatusBar status={status} submitting={submitting} />
+          <br />
+        </div>
+      )}
+      <Form>
+        <Form.Input
+          label="Room Name"
+          value={roomName}
+          onChange={e => setRoomName(e.target.value)}
+          required
+          name="roomName"
+        />
+        <Form.Select
+          options={options}
+          label="Room Category"
+          onChange={(e,data) => setRoomCategory(data.value)}
+          required
+          name="roomCategory"
+        />
+        {roomCategory === 'other' && (
+          <Form.Input
+            placeholder="New Room Category"
+            value={roomNewCategory}
+            onChange={e => setRoomNewCategory(e.target.value)}
+            required
+            name="roomNewCategory"
+          />
+        )}
+        <Form.Input
+          type="number"
+          label="Room Recommended Capacity"
+          value={roomRecommendedCapacity}
+          onChange={e => setRoomRecommendedCapacity(e.target.value)}
+          required
+          name="roomRecommendedCapacity"
+        />
+        <Form.Input
+          type="email"
+          label="Room Contact Email"
+          value={roomContactEmail}
+          onChange={e => setRoomContactEmail(e.target.value)}
+          required
+          name="roomContactEmail"
+        />
+        <Button
+          primary
+          fluid
+          disabled={!areValidFields() || success}
+          onClick={toggleConfirmation}>
+          Submit
+        </Button>
+        <Confirm
+          open={confirming}
+          onCancel={toggleConfirmation}
+          onConfirm={handleOnSubmit}
+          content="Confirm Room Creation?"
+          size="mini"
+        />
+      </Form>
+    </Container>
+  );
 }
 
 export default RoomCreationForm;
