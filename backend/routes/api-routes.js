c61ba5af9d07f8f758137da29fc8b1375ac7287e
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-01-22 00:00:34 +0800

    Finalise mobile view

--- backend/routes/api-routes.js
+++ backend/routes/api-routes.js
@@ -1,14 +1,12 @@
-const router = require('express').Router();
-const roomRoutes = require('./rooms/room-routes');
-const eventRoutes = require('./events/event-routes');
-const accountRoutes = require('./account-routes');
-const emailRoutes = require('./email-routes');
-const sheetRoutes = require('./sheet-routes');
+const router = require("express").Router();
+const roomRoutes = require("./rooms/room-routes");
+const eventRoutes = require("./events/event-routes");
+const accountRoutes = require("./account-routes");
+const emailRoutes = require("./email-routes");
 
-router.use('/rooms', roomRoutes);
-router.use('/accounts', accountRoutes);
-router.use('/emails', emailRoutes);
-router.use('/events', eventRoutes);
-router.use('/sheets', sheetRoutes);
+router.use("/rooms", roomRoutes);
+router.use("/accounts", accountRoutes);
+router.use("/emails", emailRoutes);
+router.use("/events", eventRoutes);
 
 module.exports = router;
