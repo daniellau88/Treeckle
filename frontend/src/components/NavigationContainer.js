26e696079eeeb5772354e7780c6f2aa7162ec5b0
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-01-22 00:00:34 +0800

    Finalise mobile view

--- frontend/src/components/NavigationContainer.js
+++ frontend/src/components/NavigationContainer.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Responsive } from "semantic-ui-react";
 import MobileNavigationBar from "./MobileNavigationBar";
 import DesktopNavigationBar from "./DesktopNavigationBar";
+import { CountsContext } from "../contexts/CountsProvider";
 
 function NavigationContainer({ children }) {
   const [activeTab, setActiveTab] = useState("");
+  const counts = useContext(CountsContext);
+  useEffect(() => counts.setCounts({ updater: !counts.counts.updater }), []);
 
   const getWidth = () => {
     const isSSR = typeof window === "undefined";
