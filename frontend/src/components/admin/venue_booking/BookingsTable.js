dd13f91b7676a8582da079270df3b9baf8074a25
kalsyc <klassiclegend@gmail.com>
2020-01-31 14:10:27 +0800

    Add dropdown without filtering

--- frontend/src/components/admin/venue_booking/BookingsTable.js
+++ frontend/src/components/admin/venue_booking/BookingsTable.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { Context } from "../../../contexts/UserProvider";
-import { Table, Segment } from "semantic-ui-react";
+import { Dropdown, Table, Segment } from "semantic-ui-react";
 import StatusButton from "../../common/StatusButton";
 import { toDateTimeString } from "../../../util/DateUtil";
 import { CONSOLE_LOGGING } from "../../../DevelopmentView";
@@ -14,20 +14,62 @@ class BookingsTable extends React.Component {
     super(props);
     this.state = {
       allRequests: [],
-      isLoading: true
+      isLoading: true,
+      filteredArray: [],
     };
 
+    this.options = [
+      { key: 0, text: "Pending", value: "Pending" },
+      { key: 1, text: "Approved", value: "Approved" },
+      { key: 2, text: "Rejected", value: "Rejected" },
+      { key: 3, text: "Cancelled", value: "Cancelled" }
+    ];
+
     this.renderBodyRow = this.renderBodyRow.bind(this);
+    this.renderLabel = this.renderLabel.bind(this);
     this.retrieveAllRequests = this.retrieveAllRequests.bind(this);
+    this.onLabelClick = this.onLabelClick.bind(this);
+
   }
 
   componentDidMount() {
     this.retrieveAllRequests();
   }
 
-  retrieveRejectedRequests() {
+  renderLabel(label) {
+    let labelColor;
+    switch (label.key) {
+      case 0:
+        labelColor = "orange";
+        break;
+      case 1:
+        labelColor = "green";
+        break;
+      case 2:
+        labelColor = "red";
+        break;
+      case 3:
+        labelColor = "black";
+        break;
+      default:
+        labelColor = "standard";
+    }
+
+    return {
+      color: labelColor,
+      content: label.text,
+      icon: 'check'
+    }
+
+  }
+
+  onLabelClick() {
+
+  }
+
+  retrievePendingRequests() {
     return axios.get(
-      `../api/rooms/bookings/all?Approved=0&Pending=0&Cancelled=0&limit=1000`,
+      `../api/rooms/bookings/all?Approved=0&Rejected=0&Cancelled=0&limit=1000`,
       {
         headers: { Authorization: `Bearer ${this.context.token}` }
       }
@@ -36,7 +78,16 @@ class BookingsTable extends React.Component {
 
   retrieveApprovedRequests() {
     return axios.get(
-      `../api/rooms/bookings/all?Rejected=0&Pending=0&Cancelled=0&limit=1000`,
+      `../api/rooms/bookings/all?Cancelled=0&Rejected=0&Pending=0&limit=1000`,
+      {
+        headers: { Authorization: `Bearer ${this.context.token}` }
+      }
+    );
+  }
+
+  retrieveRejectedRequests() {
+    return axios.get(
+      `../api/rooms/bookings/all?Approved=0&Cancelled=0&Pending=0&limit=1000`,
       {
         headers: { Authorization: `Bearer ${this.context.token}` }
       }
@@ -45,16 +96,7 @@ class BookingsTable extends React.Component {
 
   retrieveCancelledRequests() {
     return axios.get(
-      `../api/rooms/bookings/all?Rejected=0&Pending=0&Approved=0&limit=1000`,
-      {
-        headers: { Authorization: `Bearer ${this.context.token}` }
-      }
-    );
-  }
-
-  retrievePendingRequests() {
-    return axios.get(
-      `../api/rooms/bookings/all?Approved=0&Rejected=0&Cancelled=0&limit=1000`,
+      `../api/rooms/bookings/all?Approved=0&Rejected=0&Pending=0&limit=1000`,
       {
         headers: { Authorization: `Bearer ${this.context.token}` }
       }
@@ -154,25 +196,37 @@ class BookingsTable extends React.Component {
     return (
       <div className="scrollable-table" style={{ maxHeight: "44em" }}>
         {this.state.allRequests.length > 0 ? (
-          <Table
-            selectable
-            headerRow={
-              <Table.Row>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Email</Table.HeaderCell>
-                <Table.HeaderCell>Contact number</Table.HeaderCell>
-                <Table.HeaderCell>Venue</Table.HeaderCell>
-                <Table.HeaderCell>Start</Table.HeaderCell>
-                <Table.HeaderCell>End</Table.HeaderCell>
-                <Table.HeaderCell>Number of participants</Table.HeaderCell>
-                <Table.HeaderCell>Purpose</Table.HeaderCell>
-                <Table.HeaderCell>Submitted at</Table.HeaderCell>
-                <Table.HeaderCell>Status</Table.HeaderCell>
-              </Table.Row>
-            }
-            tableData={this.state.allRequests}
-            renderBodyRow={this.renderBodyRow}
-          />
+          <div>
+            <Dropdown
+              fluid
+              multiple
+              search
+              selection
+              options={this.options}
+              placeholder='Filter by status'
+              renderLabel={this.renderLabel}
+              onLabelClick={this.onLabelClick}
+            />
+            <Table
+              selectable
+              headerRow={
+                <Table.Row>
+                  <Table.HeaderCell>Name</Table.HeaderCell>
+                  <Table.HeaderCell>Email</Table.HeaderCell>
+                  <Table.HeaderCell>Contact number</Table.HeaderCell>
+                  <Table.HeaderCell>Venue</Table.HeaderCell>
+                  <Table.HeaderCell>Start</Table.HeaderCell>
+                  <Table.HeaderCell>End</Table.HeaderCell>
+                  <Table.HeaderCell>Number of participants</Table.HeaderCell>
+                  <Table.HeaderCell>Purpose</Table.HeaderCell>
+                  <Table.HeaderCell>Submitted at</Table.HeaderCell>
+                  <Table.HeaderCell>Status</Table.HeaderCell>
+                </Table.Row>
+              }
+              tableData={this.state.allRequests}
+              renderBodyRow={this.renderBodyRow}
+            />
+          </div>
         ) : (
           <Segment
             placeholder
